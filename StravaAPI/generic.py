3802ee98f1b18974b098a1cf5254b2cb4e7e2ac7
Struan Brown <struan@tonnard.co.uk>
2025-02-28 13:23:24 +0000

    renamed API to strava API to differentiate between it and soon to be custom api

---
+++ StravaAPI/generic.py
@@ -0,0 +1,26 @@
+# non running specific functions relating to the api
+
+import API.calls as calls
+import json
+
+def get_numner_of_activities():
+    data = calls.get_stats()
+    runs = data['all_run_totals']['count'] 
+    cycles = data['all_ride_totals']['count'] 
+    swims = data['all_swim_totals']['count']
+    
+    activities = runs + cycles + swims
+    
+    return activities
+
+def get_api_parameters():
+    with open('params.json') as f:
+        params = json.load(f)
+
+    ID = params["parameters"]["id"]
+    ClientID = params["parameters"]["client_id"]
+    ClientSecret = params["parameters"]["client_secret"]
+    RefreshToken = params["parameters"]["refresh_token"]
+    AccessToken = params["parameters"]["accesstoken"]
+    
+    return ID, ClientID, ClientSecret, RefreshToken, AccessToken
