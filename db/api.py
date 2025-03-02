bc03cf05bbcd1660577c480ab0b155d164807b27
Struan Brown <struan@tonnard.co.uk>
2025-03-02 18:13:25 +0000

    moved all direct database access logic into one file

--- db/api.py
+++ db/api.py
@@ -7,58 +7,20 @@ from StravaAPI.running import get_runs
 app = FastAPI()
 
 @app.get("/runs")
-def get_all_runs():
-    params = get_db_parameters()
-    dbname = params[0]
-    user = params[1]
-    host = params[2]
-    password = params[3]
-    port = params[4]
+def all_runs():
+    response = get_all_runs_from_db()
     
-    data =[]
-    
-    with psycopg.connect(dbname = dbname, 
-                                user = user, 
-                                host= host,
-                                password = password,
-                                port = port ) as conn:
-
-            # Open a cursor to perform database operations
-            with conn.cursor() as cur:
-                cur.execute("SELECT * FROM run")
-                for record in cur:
-                    data.append(record)
-                   
-    return data
+    return response
 
 @app.get("/run/{run_id}")
 def get_run_by_id(run_id):
-    params = get_db_parameters()
-    dbname = params[0]
-    user = params[1]
-    host = params[2]
-    password = params[3]
-    port = params[4]
+    response = get_run_from_db_by_id(run_id)
     
-    data =[]
-    
-    with psycopg.connect(dbname = dbname, 
-                                user = user, 
-                                host= host,
-                                password = password,
-                                port = port ) as conn:
-
-            # Open a cursor to perform database operations
-            with conn.cursor() as cur:
-                cur.execute("SELECT * FROM run where id= (%s)",(run_id,))
-                for record in cur:
-                    data.append(record)
-                   
-    return data
+    return response
 
 @app.get("/runs/refresh")
 def refresh_data_from_strava():
     runs = get_runs()
     write_runs_to_db(runs)
     
-    return runs
+    return "Db data refreshed"
--- db/dblogic.py
+++ db/dblogic.py
@@ -37,7 +37,55 @@ def write_runs_to_db(runs: list):
 
                 # Make the changes to the database persistent
                 conn.commit()
-                
+      
+def get_all_runs_from_db():
+    params = get_db_parameters()
+    dbname = params[0]
+    user = params[1]
+    host = params[2]
+    password = params[3]
+    port = params[4]
+    
+    data =[]
+    
+    with psycopg.connect(dbname = dbname, 
+                                user = user, 
+                                host= host,
+                                password = password,
+                                port = port ) as conn:
+
+            # Open a cursor to perform database operations
+            with conn.cursor() as cur:
+                cur.execute("SELECT * FROM run")
+                for record in cur:
+                    data.append(record)
+                   
+    return data
+
+def get_run_from_db_by_id(run_id):
+    params = get_db_parameters()
+    dbname = params[0]
+    user = params[1]
+    host = params[2]
+    password = params[3]
+    port = params[4]
+    
+    data =[]
+    
+    with psycopg.connect(dbname = dbname, 
+                                user = user, 
+                                host= host,
+                                password = password,
+                                port = port ) as conn:
+
+            # Open a cursor to perform database operations
+            with conn.cursor() as cur:
+                cur.execute("SELECT * FROM run where id= (%s)",(run_id,))
+                for record in cur:
+                    data.append(record)
+                   
+    return data
+
 def get_db_parameters():
     with open('db/dbparams.json') as f:
         params = json.load(f)
